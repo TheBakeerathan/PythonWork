0adff1b54eeaae019b9125f9739cebb91da22faf
TheBakeerathan <bakee14@yahoo.com>
2017-07-19 20:26:13 -0400

    I added functions
    using function

--- acti.py
+++ acti.py
@@ -1,7 +1,13 @@
-temp=int(input("What is temperature?"))
-if temp>60 and temp<90 :
-    print("It is warm")
-elif temp>=90 :
-    print("It is hot")
-elif temp<60:
-    print("It is chilly")
+def gettemp(temp):
+    if temp > 60 and temp < 90:
+        print("It is warm")
+    elif temp >= 90:
+        print("It is hot")
+    elif temp < 60:
+        print("It is chilly")
+
+t = int(input("What is temperature?"))
+gettemp(t)
+
+
+
