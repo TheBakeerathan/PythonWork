efde39dbf1f8ffeebbd4631f8ecca17f195239a6
TheBakeerathan <bakee14@yahoo.com>
2017-07-29 12:55:41 -0400

    quadratic formula

---
+++ quadratic.py
@@ -0,0 +1,8 @@
+a=int(input("Enter value for A:"))
+b=int(input("Enter value for B:"))
+c=int(input("Enter value for C:"))
+x=int(input("Enter value for x:"))
+quad=str(a) + "x^2+" + str(b) + "x+" + str(c)
+print("You entered this quadratic:",quad)
+ans=a*(x*x)+(b*x)+c
+print("The value for your quadratic is ",ans)
